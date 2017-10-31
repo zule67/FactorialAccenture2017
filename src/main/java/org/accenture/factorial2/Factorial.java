e0a581629fb01d93d7649630d0f551099ed8e989
zule67 <zule67@hotmail.com>
2017-10-31 20:07:54 +0100

    all ok

--- src/main/java/org/accenture/factorial2/Factorial.java
+++ src/main/java/org/accenture/factorial2/Factorial.java
@@ -3,14 +3,21 @@ package org.accenture.factorial2;
 public class Factorial {
 
 	public long compute(long value) {
-		if(value<0) {
+		long result ;
+		if (value < 0) {
 			throw new RuntimeException("The value is negative :" + value);
 		}
-		if((value == 0) || (value == 1)) {
-		return 1;
-	} else {
-		return value*compute(value - 1);
+		if ((value == 0) || (value == 1)) {
+			result = 1;
+		} else {
+			return result = value * compute(value - 1);
+		}
+		return result;
+
 	}
-	
-}
+	public String toString() {
+		int a = 5;
+		long b = a+ 5;
+		return "";
+	}
 }
